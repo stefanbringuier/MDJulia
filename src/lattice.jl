3d7f36d9d48ec9d533cad3cbd2a887af19be9f33
Stefan Bringuier <stefanb@email.arizona.edu>
2015-02-05 10:32:05 -0700

    fccubic function working not yet tested

--- src/lattice.jl
+++ src/lattice.jl
@@ -38,7 +38,7 @@ function scubic(multiple,latparam)
 end
 
 function fccubic(multiple,latparam)
-    #Generate face-centered cubic structure 
+    # Generate face-centered cubic structure 
     # multiple should be a tuple like object
     
     basis = [0.0 0.0 0.0;
@@ -48,17 +48,20 @@ function fccubic(multiple,latparam)
     N = size(basis,1)*multiple[1]*multiple[2]*multiple[3]
     
     lattice = zeros(N,3)
-    types = [ 1 for i=1:N*length(basis)]
+    types = [ 1 for i=1:N*size(basis,1)]
     
+    #TODO - reduce loops (i.e. vector operations)
     n = 1 #Atomic index
     #Origin: 0,0,0
     for i=0:multiple[1]-1
         for j=0:multiple[2]-1
             for k=0:multiple[3]-1
-                lattice[n,1] = (i+basis[1])*latparam
-                lattice[n,2] = (j+basis[2])*latparam
-                lattice[n,3] = (k+basis[3])*latparam
+                for a=1:size(basis,1)
+                lattice[n,1] = (i+basis[a,1])*latparam
+                lattice[n,2] = (j+basis[a,2])*latparam
+                lattice[n,3] = (k+basis[a,3])*latparam
                 n += 1
+                end
             end
         end
     end
